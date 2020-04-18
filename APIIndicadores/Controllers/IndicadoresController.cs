b88789bc9dcbec687f6912b4184b84cda98d60ca
Renato Groffe <renato.groffe@yahoo.com.br>
2020-04-18 08:53:47 -0300

    ASP.NET Core 3.1 + Indicadores Econômicos + Leitura do arquivo appsettings.json
    ASP.NET Core 3.1 + Indicadores Econômicos + Leitura do arquivo appsettings.json

---
+++ APIIndicadores/Controllers/IndicadoresController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using Microsoft.Extensions.Logging;
+using APIIndicadores.Models;
+
+namespace APIIndicadores.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class IndicadoresController : ControllerBase
+    {
+        [HttpGet]
+        public RelacaoIndicadores Get([FromServices]IConfiguration configuration)
+        {
+            var relacaoIndicadores = new RelacaoIndicadores();
+            new ConfigureFromConfigurationOptions<RelacaoIndicadores>(
+                configuration.GetSection("Indicadores"))
+                    .Configure(relacaoIndicadores);
+
+            return relacaoIndicadores;
+        }
+    }
+}
